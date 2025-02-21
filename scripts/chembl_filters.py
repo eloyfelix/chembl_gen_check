6cec16a11a00713f4d5f0c67537e1b508e2c957e
Eloy Felix <eloyfelix@gmail.com>
2025-02-21 08:56:22 +0000

    add lacan on chembl, add chembl structural alerts

--- scripts/chembl_filters.py
+++ scripts/chembl_filters.py
@@ -8,9 +8,14 @@ import math
 import re
 import logging
 from genval.ring_systems import RingSystemFinder
+from genval.lacan import mol_to_pairs
+from collections import Counter
 from tqdm import tqdm
+import pickle
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import as_completed
 
-RDLogger.DisableLog('rdApp.*')
+RDLogger.DisableLog("rdApp.*")
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -49,29 +54,113 @@ def download_and_extract_smiles(file_url):
     return smiles_list
 
 
-def get_unique_scaffolds(mol_list):
+def get_lacan_profile_for_mol(mol):
+    idx_counter = Counter()
+    pair_counter = Counter()
+    pairs = mol_to_pairs(mol)
+    pair_counter.update(pairs)
+    for a, b in pairs:
+        idx_counter[a] += 1
+        idx_counter[b] += 1
+
+    return idx_counter, pair_counter, len(pairs)
+
+
+def combine_lacan_profiles(profiles, size=1024):
+    idx_counter = Counter()
+    pair_counter = Counter()
+    setsize = 0
+
+    for i_count, p_count, s in profiles:
+        idx_counter.update(i_count)
+        pair_counter.update(p_count)
+        setsize += s
+
+    idx_occurrences = dict(idx_counter.most_common(size - 1))
+    return {"idx": idx_occurrences, "pairs": dict(pair_counter), "setsize": setsize}
+
+
+def get_lacan_profile_for_mols(mol_list, size=1024, n_workers=None, chunk_size=1000):
+    profiles = []
+    with ProcessPoolExecutor(max_workers=n_workers) as executor:
+        for i in tqdm(
+            range(0, len(mol_list), chunk_size),
+            desc="Processing molecules for LACAN profile",
+        ):
+            chunk = mol_list[i : i + chunk_size]
+            futures = [executor.submit(get_lacan_profile_for_mol, mol) for mol in chunk]
+            for future in futures:
+                profiles.append(future.result())
+    return combine_lacan_profiles(profiles, size)
+
+
+def get_unique_scaffolds(mol_list, n_workers=None, chunk_size=1000):
     unique_scaffolds = set()
-    for mol in tqdm(mol_list, desc="Processing scaffolds"):
-        try:
-            scaffold = MurckoScaffold.GetScaffoldForMol(mol)
-            scaffold_smiles = Chem.MolToSmiles(scaffold)
-            unique_scaffolds.add(scaffold_smiles)
-        except:
-            continue
+
+    with ProcessPoolExecutor(max_workers=n_workers) as executor:
+        futures = []
+        for i in range(0, len(mol_list), chunk_size):
+            chunk = mol_list[i : i + chunk_size]
+            futures.append(executor.submit(process_scaffold_chunk, chunk))
+
+        for future in tqdm(
+            as_completed(futures), total=len(futures), desc="Processing scaffolds"
+        ):
+            try:
+                scaffolds = future.result()
+                unique_scaffolds.update(scaffolds)
+            except Exception as e:
+                logging.error(f"Error processing a chunk: {e}")
+                continue
+
     return unique_scaffolds
 
 
-def get_unique_ring_systems(mol_list):
+def process_scaffold_chunk(mol_list):
+    scaffolds = set()
+    for mol in mol_list:
+        try:
+            scaffold = MurckoScaffold.GetScaffoldForMol(mol)
+            scaffold_smiles = Chem.MolToSmiles(scaffold)
+            scaffolds.add(scaffold_smiles)
+        except:
+            continue
+    return scaffolds
+
+
+def get_unique_ring_systems(mol_list, n_workers=None, chunk_size=1000):
     ring_finder = RingSystemFinder()
     unique_rings = set()
-    for mol in tqdm(mol_list, desc="Processing ring systems"):
+
+    with ProcessPoolExecutor(max_workers=n_workers) as executor:
+        futures = []
+        for i in range(0, len(mol_list), chunk_size):
+            chunk = mol_list[i : i + chunk_size]
+            futures.append(executor.submit(process_ring_systems, chunk, ring_finder))
+
+        for future in tqdm(
+            as_completed(futures), total=len(futures), desc="Processing ring systems"
+        ):
+            try:
+                rings = future.result()
+                unique_rings.update(rings)
+            except Exception as e:
+                logging.error(f"Error processing a chunk: {e}")
+                continue
+
+    return unique_rings
+
+
+def process_ring_systems(mol_list, ring_finder):
+    rings = set()
+    for mol in mol_list:
         try:
-            rings = ring_finder.find_ring_systems(mol, as_mols=False)
-            for r in rings:
-                unique_rings.add(r)
+            found_rings = ring_finder.find_ring_systems(mol, as_mols=False)
+            for r in found_rings:
+                rings.add(r)
         except Exception:
             continue
-    return unique_rings
+    return rings
 
 
 def calc_m(epsilon, N):
@@ -90,6 +179,11 @@ def create_bloom_filter(items, filter_id):
     logging.info(f"Bloom filter saved to {filter_id}.bloom")
 
 
+def smiles_to_mol(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    return mol
+
+
 if __name__ == "__main__":
     logging.info("Starting ChEMBL filter creation")
     file_url = get_latest_chembl_file_url()
@@ -97,16 +191,24 @@ if __name__ == "__main__":
         logging.info(f"Downloading data from {file_url}")
         smiles_list = download_and_extract_smiles(file_url)
 
-        mol_list = []
-        for smiles in tqdm(smiles_list, desc="Parsing SMILES"):
-            mol = Chem.MolFromSmiles(smiles)
-            if mol:
-                mol_list.append(mol)
+        with ProcessPoolExecutor() as executor:
+            mol_list = list(
+                tqdm(
+                    executor.map(smiles_to_mol, smiles_list, chunksize=10000),
+                    total=len(smiles_list),
+                    desc="Parsing SMILES",
+                    smoothing=0,
+                )
+            )
+        mol_list = [mol for mol in mol_list if mol is not None]
 
         unique_scaffolds = get_unique_scaffolds(mol_list)
         create_bloom_filter(unique_scaffolds, "chembl_scaffold")
 
         unique_ring_systems = get_unique_ring_systems(mol_list)
         create_bloom_filter(unique_ring_systems, "chembl_ring_system")
+        lacan_profile = get_lacan_profile_for_mols(mol_list)
+        with open("chembl_lacan.pkl", "wb") as file:
+            pickle.dump(lacan_profile, file)
     else:
         logging.error("Could not find a valid file URL.")
---
+++ src/genval/lacan.py
@@ -0,0 +1,68 @@
+from rdkit.Chem import rdFingerprintGenerator
+from rdkit import Chem
+
+MFPGEN = rdFingerprintGenerator.GetMorganGenerator(1)
+ao = rdFingerprintGenerator.AdditionalOutput()
+ao.AllocateBitInfoMap()
+ao.AllocateAtomToBits()
+
+
+def mol_to_pairs(m):
+    """
+    function that fractures every bond and reports the two ECFP2
+    (including dummy) at the fracture point.
+    """
+    id_pairs = []
+    ri_full = m.GetRingInfo()
+    ar = ri_full.AtomRings()
+    for b in m.GetBonds():
+        bidx = [b.GetBeginAtomIdx(), b.GetEndAtomIdx()]
+        newmol = Chem.FragmentOnBonds(m, [b.GetIdx()])
+        try:
+            if b.IsInRing():
+                Chem.SanitizeMol(
+                    newmol, sanitizeOps=Chem.SanitizeFlags.SANITIZE_SYMMRINGS
+                )
+                ri = newmol.GetRingInfo()  # reset ringinfo trick
+                for ring in ar:
+                    for idx in ring:
+                        ri.AddRing((idx,), (0,))
+            else:
+                Chem.SanitizeMol(newmol)
+            MFPGEN.GetSparseFingerprint(newmol, fromAtoms=bidx, additionalOutput=ao)
+            id_pairs.append(tuple(sorted([ao.GetAtomToBits()[idx][1] for idx in bidx])))
+        except Exception as e:
+            pass  # silent for now
+    return id_pairs
+
+
+def assess_per_bond(mol, profile):
+    pairs = mol_to_pairs(mol)
+    total = profile["setsize"]
+    idx = profile["idx"]
+    pair_counts = profile["pairs"]
+    results = []
+    for pair in pairs:
+        o1 = idx.get(pair[0], 0) / total / 2
+        o2 = idx.get(pair[1], 0) / total / 2
+        expected = o1 * o2
+        real = pair_counts.get(pair, 0) / total
+        results.append(0 if expected == 0 else real / expected)
+    return results
+
+
+def score_mol(mol, profile, mode="score", t=0.05):
+    apb = assess_per_bond(mol, profile)
+    if not apb:
+        apb = [0]
+    min_val = min(apb)
+    info = {"bad_bonds": [i for i, b in enumerate(apb) if b < t]}
+
+    if mode == "threshold":
+        score = 0 if min_val < t else 1
+    elif mode == "score":
+        score = min(0.5 * (min_val / t) ** 0.5, 1.0)
+    else:
+        print("mode not supported yet, sorry.")
+        score = None
+    return score, info
