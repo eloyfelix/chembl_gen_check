f86cedefd3ddc34f10441d72557e1a380050d222
Eloy Félix <eloyfelix@gmail.com>
2025-02-26 14:35:18 +0000

    Lacan (#1)
    * add lacan on chembl, add chembl structural alerts
    
    * rdkit warnings
    
    * helper function to visualise lacan bad bonds
    
    * update README

--- README.md
+++ README.md
@@ -8,9 +8,15 @@ Leveraging [molbloom](https://github.com/whitead/molbloom) filters for ChEMBL sc
 from genval import GenVal
 
 gv = GenVal()
-gv.check_scaffold("CC(=O)Oc1ccccc1C(=O)O")
-gv.check_ring_systems("CC(=O)Oc1ccccc1C(=O)O")
+
+smiles = "CC(=O)Oc1ccccc1C(=O)O"
+
+gv.check_scaffold(smiles) # Using scaffolds found in ChEMBL
+gv.check_ring_systems(smiles) # Using ring systems found in ChEMBL
+gv.check_lacan(smiles) # Profile generated using ChEMBL
+gv.check_structural_alerts(smiles) # ChEMBL set
 ```
 
+Code to extract ring systems adapted from: W Patrick Walters. [useful_rdkit_utils](https://github.com/PatWalters/useful_rdkit_utils/blob/master/useful_rdkit_utils/ring_systems.py)
 
-Code to extract ring systems comes from Patt Walters' [useful_rdkit_utils](https://github.com/PatWalters/useful_rdkit_utils/blob/master/useful_rdkit_utils/ring_systems.py)
+Code to calculate LACAN scores adapted from: Dehaen, W. LACAN. https://github.com/dehaenw/lacan/
--- src/genval/__init__.py
+++ src/genval/__init__.py
@@ -1,14 +1,27 @@
+import warnings
+# Filter out RDKit converter warnings
+warnings.filterwarnings('ignore', category=RuntimeWarning,
+                       message='to-Python converter for boost::shared_ptr.*')
+
 from importlib.resources import files
 from rdkit.Chem.Scaffolds import MurckoScaffold
 from .ring_systems import RingSystemFinder
+from .lacan import score_mol
 from molbloom import BloomFilter
+from rdkit.Chem import FilterCatalog
 from rdkit import Chem
+import pickle
+
+params = FilterCatalog.FilterCatalogParams()
+params.AddCatalog(params.FilterCatalogs.CHEMBL)
+sa_catalog = FilterCatalog.FilterCatalog(params)
 
 
 databases = {
     "chembl": {
         "scaffold": "chembl_scaffold.bloom",
         "ring_system": "chembl_ring_system.bloom",
+        "lacan_profile": "chembl_lacan.pkl",
     },
 }
 
@@ -16,12 +29,18 @@ databases = {
 class GenVal:
     scaffold_filter = None
     ring_sytem_filter = None
+    lacal_profile = None
 
     def __init__(self, db_name: str = "chembl") -> None:
         s_file_path = files("genval.data").joinpath(databases[db_name]["scaffold"])
         self.scaffold_filter = BloomFilter(str(s_file_path))
         rs_file_path = files("genval.data").joinpath(databases[db_name]["ring_system"])
         self.ring_sytem_filter = BloomFilter(str(rs_file_path))
+        lp_file_path = files("genval.data").joinpath(
+            databases[db_name]["lacan_profile"]
+        )
+        with open(lp_file_path, "rb") as file:
+            self.lacan_profile = pickle.load(file)
 
     def check_scaffold(self, smiles: str) -> bool:
         mol = Chem.MolFromSmiles(smiles)
@@ -43,8 +62,19 @@ class GenVal:
             ring_systems = ring_system_finder.find_ring_systems(mol, as_mols=False)
             for rs in ring_systems:
                 if rs not in self.ring_sytem_filter:
-                    print(rs)
                     return False
             return True
         except:
             return False
+
+    def check_structural_alerts(self, smiles):
+        mol = Chem.MolFromSmiles(smiles)
+        if not mol:
+            return False
+        return len(sa_catalog.GetMatches(mol))
+
+    def check_lacan(self, smiles):
+        mol = Chem.MolFromSmiles(smiles)
+        if not mol:
+            return False
+        return score_mol(mol, self.lacan_profile)
